6bfbace21171beb6facf8447826acf8f3e35d350
vhasus <slicehost@thoughtworks.com>
2012-05-28 10:48:46 +0530

    Handling cases when no number match found around 'years' Ignoring data directory

--- resume_experience_mapper.rb
+++ resume_experience_mapper.rb
@@ -6,11 +6,16 @@ require 'wukong'
 class ResumeExperienceMapper < Wukong::Streamer::LineStreamer
   def process line
       resume_in_single_line = line
+      puts "length of resume - #{resume_in_single_line.length}"
       index_of_years_of_experience = resume_in_single_line.index("years")
 
       if index_of_years_of_experience
+        puts "after getting index of years of experience - #{index_of_years_of_experience}"
         text_around_years_of_experience = resume_in_single_line[index_of_years_of_experience-20, 40]
-        years_of_experience = text_around_years_of_experience.match(/\d+/)[0]
+        puts "text around years - #{text_around_years_of_experience}"
+        if match = text_around_years_of_experience.match(/\d+/)
+          years_of_experience = match[0]
+        end
       end
       puts years_of_experience
   end
